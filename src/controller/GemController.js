35af20fae15812a6aab415022ffb64ce5b802333
gangardar <ggdgangardarggd@gmail.com>
2025-03-24 19:32:20 +0545

    -Sprint two Complete

--- src/controller/GemController.js
+++ src/controller/GemController.js
@@ -1,16 +1,26 @@
+import path from "path";
 import Gem from "../models/Gem.js";
+import fs from "fs";
 
 // Create a new gem
 export const createGem = async (req, res) => {
   try {
     if (!req.files || req.files.length === 0) {
-      return res.status(400).json({status: false, message :'No files uploaded.'});
+      return res
+        .status(400)
+        .json({ status: false, message: "No files uploaded." });
     }
-    const urls = req.files.map((file) => file.path)
-    console.log(urls)
-    const gem = new Gem({ ...req.body, merchantId: req.user._id, images : urls });
+    const urls = req.files.map((file) => file.path);
+    console.log(urls);
+    const gem = new Gem({
+      ...req.body,
+      merchantId: req.user._id,
+      images: urls,
+    });
     await gem.save();
-    res.status(201).json({ status: true, message: "Gem Added Successfully", data: gem });
+    res
+      .status(201)
+      .json({ status: true, message: "Gem Added Successfully", data: gem });
   } catch (error) {
     res.status(400).json({ status: false, message: error.message });
   }
@@ -24,12 +34,16 @@ export const getAllGems = async (req, res) => {
     const gems = await Gem.find()
       .skip(skip)
       .limit(limit)
-      .populate("merchantId")
+      .populate("merchantId verifierId")
       .sort("name");
     if (!gems || (typeof gems === "object" && Object.keys(gems).length === 0)) {
-      return res.status(404).send({status : false ,message : "Empty. Not Found!", data : gems}); // Send 404 if gem not found
+      return res
+        .status(404)
+        .send({ status: false, message: "Empty. Not Found!", data: gems }); // Send 404 if gem not found
     }
-    res.status(200).json({ status: true,message : "Retrived Successfully!", data: gems });
+    res
+      .status(200)
+      .json({ status: true, message: "Retrived Successfully!", data: gems });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -43,12 +57,14 @@ export const getNotDeletedGems = async (req, res) => {
     const gems = await Gem.find({ isDeleted: false })
       .skip(skip)
       .limit(limit)
-      .populate("merchantId")
+      .populate("merchantId verifierId")
       .sort("name");
     if (!gems || (typeof gems === "object" && Object.keys(gems).length === 0)) {
-      return res.status(404).json({status : false ,message : "Empty. Not Found!", data : gems}); // Send 404 if gem not found
+      return res
+        .status(404)
+        .json({ status: false, message: "Empty. Not Found!", data: gems }); // Send 404 if gem not found
     }
-    res.status(200).json({ status: true, message : "statusful", data: gems });
+    res.status(200).json({ status: true, message: "statusful", data: gems });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -59,7 +75,9 @@ export const getNotDeletedGems = async (req, res) => {
 export const getDeletedGems = async (req, res) => {
   try {
     const gems = await Gem.find({ isDeleted: true });
-    res.status(200).json({ status: true, message: "Retrived Successfully!", data: gems });
+    res
+      .status(200)
+      .json({ status: true, message: "Retrived Successfully!", data: gems });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -71,19 +89,43 @@ export const getGemById = async (req, res) => {
     const gem = await Gem.findById(req.params.id).populate("merchantId");
     if (!gem)
       return res.status(404).json({ status: false, message: "Gem not found" });
-    res.status(200).json({ status: true, message: "Retrived Successfully!", data: gem });
+    res
+      .status(200)
+      .json({ status: true, message: "Retrived Successfully!", data: gem });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
 };
 
-// Get a single gem by ID
+// Get a single gems by Merchant ID
 export const getGemByMerchnatId = async (req, res) => {
   try {
-    const gem = await Gem.find({merchantId : req.params.id, isDeleted : 'false'});
+    const gem = await Gem.find({
+      merchantId: req.params.id,
+      isDeleted: "false",
+    });
     if (!gem)
       return res.status(404).json({ status: false, message: "Gem not found" });
-    res.status(200).json({ status: true, message: "Retrived Successfully!", data: gem });
+    res
+      .status(200)
+      .json({ status: true, message: "Retrived Successfully!", data: gem });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+// Get a single gems by its verifierId
+export const getGemByVerifierId = async (req, res) => {
+  try {
+    const gem = await Gem.find({
+      verifierId: req.params.id,
+      isDeleted: "false",
+    }).populate("verifierId merchantId");
+    if (!gem)
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    res
+      .status(200)
+      .json({ status: true, message: "Retrived Successfully!", data: gem });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -92,12 +134,91 @@ export const getGemByMerchnatId = async (req, res) => {
 // Update a gem
 export const updateGem = async (req, res) => {
   try {
-    const gem = await Gem.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, runValidators: true
+    const { id } = req.params;
+
+    // Fetch the existing gem from the database
+    const existingGem = await Gem.findById(id);
+    if (!existingGem) {
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    }
+
+    // Get the existing images from the database
+    const existingImages = existingGem.images || [];
+
+    // Get the paths of newly uploaded files
+    const newImageUrls = req.files ? req.files.map((file) => file.path) : [];
+
+    // Combine existing images with new image URLs
+    const updatedImages = [...existingImages, ...newImageUrls];
+
+    // Create the updated gem object
+    const updatedGem = {
+      ...req.body, // Spread the rest of the body fields
+      images: updatedImages, // Use the combined images array
+    };
+    // Update the gem in the database
+    const gem = await Gem.findByIdAndUpdate(req.params.id, updatedGem, {
+      new: true, // Return the updated document
+      runValidators: true, // Run Mongoose validators
     });
     if (!gem)
       return res.status(404).json({ status: false, message: "Gem not found" });
-    res.status(200).json({ status: true,message : "Retrived Successfully!", data: gem });
+    res
+      .status(200)
+      .json({ status: true, message: "Updated Successfully!", data: gem });
+  } catch (error) {
+    res.status(400).json({ status: false, message: error.message });
+  }
+};
+
+// Update a gem
+export const verifyGem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {_id} = req.user
+
+    // Fetch the existing gem from the database
+    const existingGem = await Gem.findById(id);
+    if (!existingGem) {
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    }
+
+    // Update the status of the gem to "Verified"
+    const updatedGem = await Gem.findByIdAndUpdate(
+      id,
+      { status: "Verified", verifierId : _id },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedGem) {
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "Updated Successfully!",
+      data: updatedGem,
+    });
+  } catch (error) {
+    res.status(400).json({ status: false, message: error.message });
+  }
+};
+
+// Update a gem
+export const assignToMe = async (req, res) => {
+  try {
+    req.body.verifierId = req.user._id;
+    const gem = await Gem.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!gem)
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    res.status(200).json({
+      status: true,
+      message: `Assigned to ${req.user.username} Successfully!`,
+      data: gem,
+    });
   } catch (error) {
     res.status(400).json({ status: false, message: error.message });
   }
@@ -111,7 +232,7 @@ export const deleteGem = async (req, res) => {
       return res.status(404).json({ status: false, message: "Gem not found" });
     res
       .status(200)
-      .json({ status: true, message: "Gem deleted statusfully", data : gem });
+      .json({ status: true, message: "Gem deleted statusfully", data: gem });
   } catch (error) {
     res.status(500).json({ status: false, message: error.message });
   }
@@ -134,6 +255,61 @@ export const softDeleteGem = async (req, res) => {
   }
 };
 
+export const deleteGemImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { url } = req.body;
+
+    // Find the gem by ID
+    const gem = await Gem.findById(id);
+    if (!gem) {
+      return res.status(404).json({ status: false, message: "Gem not found" });
+    }
+    const filename = path.basename(url);
+
+    // Define the file path
+    const filePath = path.join("./storage/public/", filename);
+    console.log(filePath);
+
+    // Check if the image exists in the images array
+    if (!gem.images.includes(filePath)) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Image not found" });
+    }
+
+    // Extract the filename from the URL
+
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      // Delete the file
+      fs.unlinkSync(filePath);
+    } else {
+      return res
+        .status(404)
+        .json({ status: false, message: "File not found on server" });
+    }
+
+    // Filter out the image with the specified URL
+    gem.images = gem.images.filter((img) => img !== filePath);
+
+    console.log(gem);
+
+    // Save the updated gem to the database
+    await gem.save();
+
+    // Respond with success message and updated gem
+    res.status(200).json({
+      status: true,
+      message: "Image removed successfully",
+      data: gem,
+    });
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
 // restore deleted gem.
 export const restoreGem = async (req, res) => {
   try {
@@ -142,8 +318,8 @@ export const restoreGem = async (req, res) => {
       { isDeleted: false },
       { new: true }
     );
-    if (!gem){
-        return res.status(404).json({ status: false, message: "Gem not found" });
+    if (!gem) {
+      return res.status(404).json({ status: false, message: "Gem not found" });
     }
     res.status(200).json({ status: true, message: "Gem restored", data: gem });
   } catch (error) {
--- src/routes/gemRoute.js
+++ src/routes/gemRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { validateGem } from '../validator/validateGem.js';
-import { createGem, deleteGem, getAllGems, getDeletedGems, getGemById, getGemByMerchnatId, getNotDeletedGems, restoreGem, softDeleteGem, updateGem } from '../controller/GemController.js';
+import { assignToMe, createGem, deleteGem, deleteGemImage, getAllGems, getDeletedGems, getGemById, getGemByMerchnatId, getGemByVerifierId, getNotDeletedGems, restoreGem, softDeleteGem, updateGem, verifyGem } from '../controller/GemController.js';
 import { upload } from '../middleware/uploadImages.js';
 import multer from 'multer';
 
@@ -12,7 +12,11 @@ gemRoute.get("/", getNotDeletedGems); // Get only non-deleted gems
 gemRoute.get("/deleted", getDeletedGems); // Get only deleted gems
 gemRoute.get("/:id", getGemById);
 gemRoute.get("/merchant/:id", getGemByMerchnatId);
-gemRoute.put("/:id", validateGem, updateGem);
+gemRoute.get("/assigned/:id", getGemByVerifierId);
+gemRoute.put("/:id",upload.array("images",10), validateGem, updateGem);
+gemRoute.put("/verify/:id", verifyGem);
+gemRoute.put("/assignToMe/:id", assignToMe);
 gemRoute.delete("/:id", softDeleteGem); // Soft delete
+gemRoute.delete("/image/:id", deleteGemImage); // Soft delete
 gemRoute.put("/:id/restore", restoreGem); // Restore soft deleted gem
 export default gemRoute
